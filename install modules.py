8821222c0e57174f85947e86dcf18ea241f63df2
Sam <56645449+reigningarrow@users.noreply.github.com>
2020-09-29 18:05:14 +0100

    added jinja2 install

---
+++ install modules.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 29 15:52:00 2020
+
+@author: Sam
+"""
+
+
+import subprocess
+import sys
+import time
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    
+    
+modules=['pandas','numpy','tkinter','scipy','matplotlib','os','time','jinja2']
+for module in modules:
+    try:
+        install(module)
+    except:
+        print(f'Can\'t install {module}')
+    time.sleep(5)
